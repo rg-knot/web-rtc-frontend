d96f060236c4531fa6fea0a9af49df88d189459a
rajat <rajat@hood.live>
2025-12-17 11:55:36 +0530

    added turn server

--- public/main.js
+++ public/main.js
@@ -30,6 +30,11 @@ let peerConnection = null;
 const rtcConfig = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
+    {
+        urls: "turn:34.131.190.182:3478",
+        username: "webrtc_user",
+        credential: "webrtc_pass"
+    }
   ],
 };
 
